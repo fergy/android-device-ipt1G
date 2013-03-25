e234a1c04d7cbc39307dd05da237fd02a29e5b78
fergy <ramon.rebersak@gmail.com>
2013-03-25 16:53:23 +0100

    Added gpio keys, alsa config and removed old files

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := gpio-keys.kcm
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_KEY_CHAR_MAP)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
